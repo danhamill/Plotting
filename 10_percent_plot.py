c685818565947e266c0a42e4bab36d08bba02e48
danhamill <hamill.daniel@gmail.com>
2016-09-14 10:57:23 -0700

    cleaned up plots, added base imagery, set tex levels

--- 10_percent_plot.py
+++ 10_percent_plot.py
@@ -108,148 +108,150 @@ tex_levels=[0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.60,0.65,0.70,0.7
        1.8,1.85,1.9,1.95,2.0,2.05,2.1,2.15,2.2,2.25,2.3,2.35,2.4]
 ss_level=[0,2.5,5,7.5,10,12.5,15,17.5,20,22.5,25,27.5,30,32.5,35]
        
-fig = plt.figure(figsize=(12,24))
-ax = fig.add_subplot(3,5,11)
+fig = plt.figure(figsize=(15,6))
+ax = fig.add_subplot(1,5,1)
 ax.set_title('50 square pixel')
 m = Basemap(projection='merc', 
             epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
+            llcrnrlon=np.min(glon-0.0009), 
+            llcrnrlat=np.min(glat-0.0006),
+            urcrnrlon=np.max(glon+0.0009), 
+            urcrnrlat=np.max(glat+0.0009))
+m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['0'], xpixels=1000)
 x,y = m.projtran(glon, glat)
 im = m.contourf(x,y,ss_data_50.T, cmap='Greys_r',levels=ss_level)
-im2 = m.contourf(x, y, tex_data_50.T, alpha=0.4, cmap='YlOrRd')#levels=tex_levels
+im2 = m.contourf(x, y, tex_data_50.T, alpha=0.4, cmap='YlOrRd', levels=tex_levels)#levels=tex_levels
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1)
 cax2 = divider.append_axes("right", size="5%", pad=0.3)
 cbr = plt.colorbar(im, cax=cax)
 cbr2 = plt.colorbar(im2,cax=cax2)
 
-ax_1 = fig.add_subplot(3,5,6)
-ax_1.set_title('50 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, tex_data_50.T, alpha=0.4, cmap='YlOrRd')
-divider = make_axes_locatable(ax_1)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
+#ax_1 = fig.add_subplot(3,5,6)
+#ax_1.set_title('50 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, tex_data_50.T, alpha=0.4, cmap='YlOrRd')
+#divider = make_axes_locatable(ax_1)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
+#
+#ax_2 = fig.add_subplot(3,5,1)
+#ax_2.set_title('50 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
+#divider = make_axes_locatable(ax_2)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
 
-ax_2 = fig.add_subplot(3,5,1)
-ax_2.set_title('50 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
-divider = make_axes_locatable(ax_2)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
-
-ax1 = fig.add_subplot(3,5,12)
+ax1 = fig.add_subplot(1,5,2)
 ax1.set_title('70 square pixel')
 m1 = Basemap(projection='merc', 
             epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-
+            llcrnrlon=np.min(glon-0.0009), 
+            llcrnrlat=np.min(glat-0.0006),
+            urcrnrlon=np.max(glon+0.0009), 
+            urcrnrlat=np.max(glat+0.0009))
+m1.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['0'], xpixels=1000)
 x,y = m1.projtran(glon, glat)
-
 im = m1.contourf(x,y,ss_data_70.T, cmap='Greys_r',levels=ss_level)
-im2 = m1.contourf(x, y, tex_data_70.T, alpha=0.4, cmap='YlOrRd')#levels=tex_levels
+im2 = m1.contourf(x, y, tex_data_70.T, alpha=0.4, cmap='YlOrRd',levels=tex_levels)
 divider = make_axes_locatable(ax1)
 cax = divider.append_axes("right", size="5%", pad=0.1)
 cax2 = divider.append_axes("right", size="5%", pad=0.3)
 cbr = plt.colorbar(im, cax=cax)
 cbr2 = plt.colorbar(im2,cax=cax2)
 
-ax1_1 = fig.add_subplot(3,5,7)
-ax1_1.set_title('70 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, tex_data_70.T, alpha=0.4, cmap='YlOrRd')
-divider = make_axes_locatable(ax1_1)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
+#ax1_1 = fig.add_subplot(3,5,7)
+#ax1_1.set_title('70 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, tex_data_70.T, alpha=0.4, cmap='YlOrRd')
+#divider = make_axes_locatable(ax1_1)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
+#
+#ax1_2 = fig.add_subplot(3,5,2)
+#ax1_2.set_title('70 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, ss_data_70.T, cmap='Greys_r',levels=ss_level)
+#divider = make_axes_locatable(ax1_2)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
 
-ax1_2 = fig.add_subplot(3,5,2)
-ax1_2.set_title('70 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, ss_data_70.T, cmap='Greys_r',levels=ss_level)
-divider = make_axes_locatable(ax1_2)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
-
-ax2 = fig.add_subplot(3,5,13)
+ax2 = fig.add_subplot(1,5,3)
 ax2.set_title('80 square pixel')
 m = Basemap(projection='merc', 
             epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
+            llcrnrlon=np.min(glon-0.0009), 
+            llcrnrlat=np.min(glat-0.0006),
+            urcrnrlon=np.max(glon+0.0009), 
+            urcrnrlat=np.max(glat+0.0009))
+m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['0'], xpixels=1000)
 x,y = m.projtran(glon, glat)
 im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
-im2 = m.contourf(x, y, tex_data_80.T, alpha=0.4, cmap='YlOrRd')#levels=tex_levels
+im2 = m.contourf(x, y, tex_data_80.T, alpha=0.4, cmap='YlOrRd',levels=tex_levels)
 divider = make_axes_locatable(ax2)
 cax = divider.append_axes("right", size="5%", pad=0.1)
 cax2 = divider.append_axes("right", size="5%", pad=0.3)
 cbr = plt.colorbar(im, cax=cax)
 cbr2 = plt.colorbar(im2,cax=cax2)
 
-ax2_1 = fig.add_subplot(3,5,8)
-ax2_1.set_title('80 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, tex_data_80.T, alpha=0.4, cmap='YlOrRd')
-divider = make_axes_locatable(ax2_1)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
+#ax2_1 = fig.add_subplot(3,5,8)
+#ax2_1.set_title('80 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, tex_data_80.T, alpha=0.4, cmap='YlOrRd')
+#divider = make_axes_locatable(ax2_1)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
+#
+#ax2_2 = fig.add_subplot(3,5,3)
+#ax2_2.set_title('80 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
+#divider = make_axes_locatable(ax2_2)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
 
-ax2_2 = fig.add_subplot(3,5,3)
-ax2_2.set_title('80 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
-divider = make_axes_locatable(ax2_2)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
-
-ax3 = fig.add_subplot(3,5,14)
+ax3 = fig.add_subplot(1,5,4)
 ax3.set_title('120 square pixel')
 m = Basemap(projection='merc', 
             epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
+            llcrnrlon=np.min(glon-0.0009), 
+            llcrnrlat=np.min(glat-0.0006),
+            urcrnrlon=np.max(glon+0.0009), 
+            urcrnrlat=np.max(glat+0.0009))
+m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['0'], xpixels=1000)
 im = m.contourf(x,y,ss_data_50.T, cmap='Greys_r',levels=ss_level)
-im2 = m.contourf(x, y, tex_data_120.T, alpha=0.4, cmap='YlOrRd')#levels=tex_levels
+im2 = m.contourf(x, y, tex_data_120.T, alpha=0.4, cmap='YlOrRd',levels=tex_levels)
 divider = make_axes_locatable(ax3)
 cax = divider.append_axes("right", size="5%", pad=0.1)
 cax2 = divider.append_axes("right", size="5%", pad=0.3)
@@ -257,76 +259,76 @@ cbr = plt.colorbar(im, cax=cax)
 cbr2 = plt.colorbar(im2,cax=cax2)
 
 
-ax3_1 = fig.add_subplot(3,5,9)
-ax3_1.set_title('120 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, tex_data_120.T, alpha=0.4, cmap='YlOrRd')
-divider = make_axes_locatable(ax3_1)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
+#ax3_1 = fig.add_subplot(3,5,9)
+#ax3_1.set_title('120 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, tex_data_120.T, alpha=0.4, cmap='YlOrRd')
+#divider = make_axes_locatable(ax3_1)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
+#
+#ax3_2 = fig.add_subplot(3,5,4)
+#ax3_2.set_title('120 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
+#divider = make_axes_locatable(ax3_2)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
 
-ax3_2 = fig.add_subplot(3,5,4)
-ax3_2.set_title('120 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
-divider = make_axes_locatable(ax3_2)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
-
-ax4 = fig.add_subplot(3,5,15)
+ax4 = fig.add_subplot(1,5,5)
 ax4.set_title('160 square pixel')
 m = Basemap(projection='merc', 
             epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
+            llcrnrlon=np.min(glon-0.0009), 
+            llcrnrlat=np.min(glat-0.0006),
+            urcrnrlon=np.max(glon+0.0009), 
+            urcrnrlat=np.max(glat+0.0009))
+m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['0'], xpixels=1000)
 im = m.contourf(x,y,ss_data_50.T, cmap='Greys_r',levels=ss_level)
-im2 = m.contourf(x, y, tex_data_160.T, alpha=0.4, cmap='YlOrRd')#levels=tex_levels
+im2 = m.contourf(x, y, tex_data_160.T, alpha=0.4, cmap='YlOrRd',levels=tex_levels)
 divider = make_axes_locatable(ax4)
 cax = divider.append_axes("right", size="5%", pad=0.1)
 cax2 = divider.append_axes("right", size="5%", pad=0.3)
 cbr = plt.colorbar(im, cax=cax)
 cbr2 = plt.colorbar(im2,cax=cax2)
 
-
-
-ax4_1 = fig.add_subplot(3,5,10)
-ax4_1.set_title('160 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, tex_data_160.T, alpha=0.4, cmap='YlOrRd')
-divider = make_axes_locatable(ax4_1)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
-
-ax4_2 = fig.add_subplot(3,5,5)
-ax4_2.set_title('160 square pixel')
-m = Basemap(projection='merc', 
-            epsg=cs2cs_args.split(':')[1], 
-            llcrnrlon=np.min(glon), 
-            llcrnrlat=np.min(glat),
-            urcrnrlon=np.max(glon), 
-            urcrnrlat=np.max(glat))
-im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
-divider = make_axes_locatable(ax4_2)
-cax = divider.append_axes("right", size="5%", pad=0.1)
-cbr = plt.colorbar(im,cax=cax)
+#ax4_1 = fig.add_subplot(3,5,10)
+#ax4_1.set_title('160 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, tex_data_160.T, alpha=0.4, cmap='YlOrRd')
+#divider = make_axes_locatable(ax4_1)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
+#
+#ax4_2 = fig.add_subplot(3,5,5)
+#ax4_2.set_title('160 square pixel')
+#m = Basemap(projection='merc', 
+#            epsg=cs2cs_args.split(':')[1], 
+#            llcrnrlon=np.min(glon), 
+#            llcrnrlat=np.min(glat),
+#            urcrnrlon=np.max(glon), 
+#            urcrnrlat=np.max(glat))
+#im = m.contourf(x, y, ss_data_50.T, cmap='Greys_r',levels=ss_level)
+#divider = make_axes_locatable(ax4_2)
+#cax = divider.append_axes("right", size="5%", pad=0.1)
+#cbr = plt.colorbar(im,cax=cax)
 fig.tight_layout()
+#plt.show()
 plt.savefig(r"C:\workspace\Merged_SS\window_analysis\10_percent_shift\output\ss_tex_overlay.png",dpi=1000)
 
 
