c9cd1ea63fe81a0c4d14144d76b8a7e9e256c70a
danhamill <hamill.daniel@gmail.com>
2016-09-13 18:07:30 -0700

    Added stats, changed layout

--- raster_shp_plotter.py
+++ raster_shp_plotter.py
@@ -18,7 +18,28 @@ import pandas as pd
 from pandas.tools.plotting import table
 from rasterstats import zonal_stats
 from osgeo import ogr
+from scipy import stats
 
+def make_df(x):
+    df = pd.DataFrame(x.flatten())
+    shp = df.shape
+    return df
+def mean(obs):
+    return (1. / len(obs)) * np.sum(obs)
+
+def variance(obs):
+    return (1. / len(obs)) * np.sum((obs - mean(obs)) ** 2)
+    
+def mykurt(x):
+    num = np.sum((x - mean(x)) ** 4)/ len(x)
+    denom = variance(x) ** 2
+    return num / denom
+    
+def myskew(x):
+    df = make_df(x)
+    skew = df.skew().values
+    return float(skew[0])
+    
 
 ss_raster = r"C:\workspace\Merged_SS\window_analysis\raster\ss_10_rasterclipped.tif"
 ds = gdal.Open(ss_raster)
@@ -45,9 +66,11 @@ del ds
 #Summary Statistics Table 
 in_shp = r"C:\workspace\Merged_SS\window_analysis\shapefiles\tex_seg_800.shp"
 stats = zonal_stats(in_shp ,ss_raster,
-                    stats=['min','max','median','std','count'])
+                    stats=['min','mean','max','median','std','count','percentile_25','percentile_50','percentile_75'],
+                    add_stats = {'kurt':mykurt,'skew':myskew})
 
 df = pd.DataFrame(stats)
+df.rename(columns={'percentile_25': '25%', 'percentile_50': '50%', 'percentile_75':'75%'}, inplace=True) 
 
 #Lets get get the substrate+codes
 ds = ogr.Open(in_shp)
@@ -61,8 +84,9 @@ del ds
 df['substrate']=a
 df['std_error']=df['std']/np.sqrt(df['count'])
 
-sub = df[['substrate','std_error']]
-pivot_table = pd.pivot_table(sub,values='std_error',index='substrate',aggfunc=np.average).sort_values()
+sub = df[['substrate','mean','std','std_error','25%','50%','75%','kurt','skew']]
+pivot_table = pd.pivot_table(sub, values=['std_error','mean','std','std_error','25%','50%','75%','kurt','skew'], 
+                             index='substrate',aggfunc=np.average).sort_values(['std_error'])
 
 
 nrows, ncols = 8, 2
@@ -78,8 +102,9 @@ glon, glat = trans(xx, yy, inverse=True)
 cs2cs_args = "epsg:26949"
 ss_level=[0,2.5,5,7.5,10,12.5,15,17.5,20,22.5,25,27.5,30,32.5,35]
 
-fig = plt.figure(figsize=(6,9))
-ax = fig.add_subplot(1,2,1)
+fig = plt.figure(figsize=(12,10))
+#ax = fig.add_subplot(2,2,1)
+ax = plt.subplot2grid((3,2),(0,0),rowspan=3)
 ax.set_title('April 2014')
 m = Basemap(projection='merc', 
             epsg=cs2cs_args.split(':')[1], 
@@ -115,39 +140,52 @@ for info, shape in zip(m.layer_info, m.layer):
     if info['substrate'] == 'boulders':
         b_patch.append(Polygon(np.asarray(shape),True))
     if info['substrate'] == 'boulder':
-        b.append(Polygon(np.asarray(shape),True))  
-ax.add_collection(PatchCollection(s_patch, facecolor = '#f7fcfd',alpha=0.4, edgecolor='none',zorder=10))#,alpha=0.4
-ax.add_collection(PatchCollection(sg_patch, facecolor = '#e5f5f9',alpha=0.4, edgecolor='none',zorder=10))#,alpha=0.4
-ax.add_collection(PatchCollection(gs_patch, facecolor = '#ccece6',alpha=0.4, edgecolor='none',zorder=10))    # ,alpha=0.4
-ax.add_collection(PatchCollection(l_patch, facecolor = '#99d8c9',alpha=0.4, edgecolor='none',zorder=10)) #,alpha=0.4
-ax.add_collection(PatchCollection(g_patch, facecolor = '#66c2a4',alpha=0.4, edgecolor='none',zorder=10)) #,alpha=0.4
-ax.add_collection(PatchCollection(gb_patch, facecolor = '#41ae76',alpha=0.4, edgecolor='none',zorder=10)) #,alpha=0.4
-ax.add_collection(PatchCollection(b_patch, facecolor = '#238b45',alpha=0.4, edgecolor='none',zorder=10)) #,alpha=0.4
-ax.add_collection(PatchCollection(b, facecolor = '#005824',alpha=0.4, edgecolor='none',zorder=10)) #alpha=0.4,
-   
+        b.append(Polygon(np.asarray(shape),True))
+        
+a_val = 0.6
+colors = ['#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#01665e']
+ax.add_collection(PatchCollection(s_patch, facecolor = colors[0],alpha=a_val, edgecolor='none',zorder=10))#,alpha=0.4
+ax.add_collection(PatchCollection(sg_patch, facecolor = colors[1],alpha=a_val, edgecolor='none',zorder=10))#,alpha=0.4
+ax.add_collection(PatchCollection(gs_patch, facecolor = colors[2],alpha=a_val, edgecolor='none',zorder=10))    # ,alpha=0.4
+ax.add_collection(PatchCollection(l_patch, facecolor = colors[3],alpha=a_val, edgecolor='none',zorder=10)) #,alpha=0.4
+ax.add_collection(PatchCollection(g_patch, facecolor = colors[4],alpha=a_val, edgecolor='none',zorder=10)) #,alpha=0.4
+ax.add_collection(PatchCollection(gb_patch, facecolor = colors[5],alpha=a_val, edgecolor='none',zorder=10)) #,alpha=0.4
+ax.add_collection(PatchCollection(b_patch, facecolor = colors[6],alpha=a_val, edgecolor='none',zorder=10)) #,alpha=0.4
+ax.add_collection(PatchCollection(b, facecolor = colors[7],alpha=a_val, edgecolor='none',zorder=10)) #alpha=0.4,
+
 #create legend
-circ1 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#f7fcfd",alpha=0.4)
-circ2 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#e5f5f9",alpha=0.4)
-circ3 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#ccece6",alpha=0.4)
-circ4 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#99d8c9",alpha=0.4)
-circ5 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#66c2a4",alpha=0.4)
-circ6 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#41ae76",alpha=0.4)
-circ7 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#238b45",alpha=0.4)
-circ8 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor="#005824",alpha=0.4)
+circ1 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[0],alpha=a_val)
+circ2 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[1],alpha=a_val)
+circ3 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[2],alpha=a_val)
+circ4 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[3],alpha=a_val)
+circ5 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[4],alpha=a_val)
+circ6 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[5],alpha=a_val)
+circ7 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[6],alpha=a_val)
+circ8 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[7],alpha=a_val)
 
 
-ax1 = fig.add_subplot(1,2,2)
+#ax1 = fig.add_subplot(2,2,2)
+ax1 = plt.subplot2grid((3,2),(0,1))
 ax1.xaxis.set_visible(False)
 ax1.yaxis.set_visible(False)
 #hide the spines
 for sp in ax1.spines.itervalues():
     sp.set_color('w')
     sp.set_zorder(0)
-the_table = table(ax1, pivot_table.round(3),loc='center',colWidths=[0.3])
-the_table.set_fontsize(16)
+
   
 ax1.legend((circ1, circ2, circ3,circ4,circ5, circ6,circ7, circ8), ("sand", "sand/gravel", "gravel/sand","ledge","gravel","gravel/boulders","boulders","boulder"), 
-           numpoints=1,bbox_to_anchor=(0.3, 0.9), loc=0, borderaxespad=0.)   
-plt.tight_layout(w_pad = 12)
+           numpoints=1, loc='center left', borderaxespad=0.)   #bbox_to_anchor=(0.3, 0.9),
+
+#ax2 = fig.add_subplot(2,2,3)
+ax2 = plt.subplot2grid((3,2),(1,1),colspan=2)
+ax2.xaxis.set_visible(False)
+ax2.yaxis.set_visible(False)
+for sp in ax2.spines.itervalues():
+    sp.set_color('w')
+    sp.set_zorder(0)
+the_table = table(ax2, pivot_table.round(3),loc='center left',colWidths=[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1])
+the_table.set_fontsize(10)
+plt.tight_layout(w_pad=10)#w_pad = 
 plt.savefig(r"C:\workspace\Merged_SS\window_analysis\output\tex_seg_raster_table.png", dpi=1000)
 #plt.show()
